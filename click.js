9cc0f383dcf95ecbb5a73f70174ba4092a413377
Isaque Lopes <72939438+IsaqueLopes91@users.noreply.github.com>
2021-01-13 16:26:55 -0300

    Add files via upload

---
+++ click.js
@@ -0,0 +1,8 @@
+function clickA() {
+    document.getElementById("myImg").src = "IMG/words_P/a.png";
+        document.getElementById("text").innerHTML = "あ";
+}
+function clickAJ() {
+    document.getElementById("myImg").src = "IMG/words_J/a.png";
+        document.getElementById("text").innerHTML = "ka";
+}
